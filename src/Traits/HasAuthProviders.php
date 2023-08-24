a39dd85614ab7359db470267fcca8f055babc18f
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-08-24 13:01:57 +0100

    WIP

--- src/Traits/HasAuthProviders.php
+++ src/Traits/HasAuthProviders.php
@@ -5,11 +5,23 @@ namespace CodebarAg\LaravelAuth\Traits;
 use CodebarAg\LaravelAuth\Enums\ProviderEnum;
 use CodebarAg\LaravelAuth\Models\AuthProvider;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notification;
 
 trait HasAuthProviders
 {
     public function authProviders(): HasMany
     {
         return $this->hasMany(AuthProvider::class);
+    }
+
+    public function routeNotificationForMail(Notification $notification): array|string
+    {
+        if (!$this->authProviders()->exists()) {
+            $notification->id = $this->email;
+            return $this->email;
+        }
+
+        $notification->id = $this->authProviders()->first()->email;
+        return $this->authProviders()->first()->email;
     }
 }
