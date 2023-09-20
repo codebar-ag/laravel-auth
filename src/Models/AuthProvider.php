6ccaa506d0f47a249a1b413f85a62973a055c757
Sebastian Fix <sebastian.fix@codebar.ch>
2023-09-20 14:05:48 +0200

    WIP

--- src/Models/AuthProvider.php
+++ src/Models/AuthProvider.php
@@ -2,19 +2,27 @@
 
 namespace CodebarAg\LaravelAuth\Models;
 
-use App\Models\User;
 use CodebarAg\LaravelAuth\Enums\ProviderEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AuthProvider extends Model
 {
+    protected Model $model;
+
     protected $casts = [
         'provider' => ProviderEnum::class,
     ];
 
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->model = config('laravel-auth.model');
+    }
+
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo($this->model::class);
     }
 }
